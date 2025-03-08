f71828fd11c221ad892834e590348bab2f74d735
jocodes-alt <yousseflad6@gmail.com>
2025-03-08 16:24:50 +0000

    commit

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -18,3 +18,11 @@ function checkCountValue(){
 }
 
 }
+// Get the button element
+const button = document.getElementById('myButton');
+// Add event listener for 'click' event
+button.addEventListener('click', handleButtonClick);
+// Function to handle button click
+function handleButtonClick() {
+  console.log('Button clicked!');
+}
